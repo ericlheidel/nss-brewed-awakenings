9f1e3588681eb91e874c1aafcc6372462039c42e
Eric Heidel <ericleeheidel@yahoo.com>
2023-11-29 15:24:18 -0600

    Completed Project

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,12 +1,35 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const employeeClicked = clickEvent.target
+
+        let numberOfSales = 0
+
+        if (employeeClicked.dataset.type === "employee") {
+            for (const order of orders) {
+                if (parseInt(employeeClicked.dataset.id) === order.employeeId) {
+                    numberOfSales++
+                }
+            } window.alert(`${employeeClicked.dataset.name} has sold ${numberOfSales} products.`)
+        }
+            
+    }
+)
 
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `<li
+                    data-type="employee"
+                    data-id="${employee.id}"
+                    data-name="${employee.name}"
+                    >${employee.name}</li>`
     }
 
     html += "</ul>"
--- scripts/Products.js
+++ scripts/Products.js
@@ -2,11 +2,27 @@ import { getProducts } from "./database.js"
 
 const products = getProducts()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const productClicked = clickEvent.target
+
+        if (productClicked.dataset.type === "product") {
+            window.alert(`${productClicked.dataset.name} costs $${productClicked.dataset.price}`)
+        }
+    }
+)
+
 export const Products = () => {
     let html = "<ul>"
 
     for (const product of products) {
-        html += `<li>${product.name}</li>`
+        html += `<li
+                    data-type="product"
+                    data-name="${product.name}"
+                    data-price="${product.price.toFixed(2)}"
+                    >${product.name}
+                </li>`
     }
 
     html += "</ul>"
